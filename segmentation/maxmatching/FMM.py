be8a8c693ac1faea8b214ec253db7671576456a4
shgang <shgang97@163.com>
2020-06-20 17:31:00 +0800

    add segmentation-maxmatching-FMM

---
+++ segmentation/maxmatching/FMM.py
@@ -0,0 +1,61 @@
+# 使用最大正向匹配算法实现中文分词
+
+
+def init(filename):
+    """
+    读取字典文件
+    载入词典
+    :return:
+    """
+    words_dic = []
+    with open(filename, 'r', encoding='utf-8') as dic_input:
+        for word in dic_input:
+            words_dic.append(word.strip())
+    return set(words_dic)
+
+
+# 实现正向匹配算法中的分词方法
+def cut_word(raw_sentence, words_dic):
+    # 统计词典中最长的词
+    max_length = max(len(word) for word in words_dic)
+    sentence = raw_sentence.strip()
+    # 统计序列长度
+    words_length = len(sentence)
+    # 存储切分好的词语
+    cut_word_list = []
+    while words_length > 0:
+        max_cut_length = min(max_length, words_length)
+        subSentence = sentence[0:max_cut_length]
+        while max_cut_length > 0:
+            if subSentence in words_dic:
+                cut_word_list.append(subSentence)
+                break
+            elif max_cut_length == 1:
+                cut_word_list.append(subSentence)
+                break
+            else:
+                max_cut_length = max_cut_length - 1
+                subSentence = subSentence[0:max_cut_length]
+        sentence = sentence[max_cut_length:]
+        words_length = words_length - max_cut_length
+    words = "/".join(cut_word_list)
+    return words
+
+
+# 进行交互式测试
+def main():
+    """
+    用于交互接口
+    :return:
+    """
+    words_dic = init('dic.txt')
+    while True:
+        print("请输入您要分词的序列：")
+        input_str = input()
+        if not input_str:
+            break
+        result = cut_word(input_str, words_dic)
+        print("分词结果为：" + result)
+
+
+main()
